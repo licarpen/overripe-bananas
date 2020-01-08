edabd91d58ee97ce6d76ba1382909678010b7941
Lisa Carpenter <carpenter.lisa@gmail.com>
2020-01-08 15:44:48 -0800

    refactor get reviews and get 100 reviews test

--- __tests__/reviews.test.js
+++ __tests__/reviews.test.js
@@ -1,72 +1,57 @@
-require('dotenv').config();
+const { getReview, getFilm, getReviewer } = require('../lib/helpers/data-helpers');
 
 const request = require('supertest');
 const app = require('../lib/app');
-const connect = require('../lib/utils/connect');
-const mongoose = require('mongoose');
-const Reviewer = require('../lib/models/Reviewer');
+const chance = require('chance').Chance();
 const Review = require('../lib/models/Review');
-const Film = require('../lib/models/Film');
-const Studio = require('../lib/models/Studio');
 
-describe('app routes', () => {
-  beforeAll(() => {
-    connect();
-  });
+describe('review routes', () => {
 
-  beforeEach(() => {
-    return mongoose.connection.dropDatabase();
-  });
-
-  let myReviewer;
-  let myReview;
-  let myFilm;
-  let myStudio;
-
-  beforeEach(async() => {
-    myReviewer = await Reviewer.create({
-      name: 'Reviewer1',
-      company: 'Review Company'
-    });    
-    myStudio = await Studio.create({
-      name: 'ABC Studios'
-    });
-    myFilm = await Film.create({
-      title: 'Go Bananas!',
-      studio: myStudio._id,
-      released: 1999,
-      cast: []
-    });
-    myReview = await Review.create({ 
-      rating: 2,
-      reviewer: myReviewer._id,
-      review: 'Don\'t make the mistake of slipping on this rotten banana peel.',
-      film: myFilm._id
-    });
-  });
-
-  afterAll(() => {
-    return mongoose.connection.close();
-  });
-
-  it('creates a review', async() => {
+  it('creates a review', async() => {  
+    const reviewer = await getReviewer();
+    const film = await getFilm();
     return request(app)
       .post('/api/v1/reviews')
       .send({
         rating: 2,
-        reviewer: myReviewer._id,
+        reviewer: reviewer._id,
         review: 'Don\'t make the mistake of slipping on this rotten banana peel.',
-        film: myFilm._id
+        film: film._id
       })
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.any(String),
           __v: 0,
           rating: 2,
-          reviewer: myReviewer._id.toString(),
+          reviewer: reviewer._id.toString(),
           review: 'Don\'t make the mistake of slipping on this rotten banana peel.',
-          film: myFilm._id.toString()
+          film: film._id.toString()
         });
       });
+  });
+  it('gets 100 reviews when there are more than 100 reviews', async() => {
+    const reviewer = await getReviewer();
+    const film = await getFilm();
+    
+    await Review.create([...Array(101)].map(() => ({
+      rating: 4,
+      reviewer: reviewer._id,
+      review: chance.string({ length: 140 }),
+      film: film._id
+    })));
+    return request(app)
+      .get('/api/v1/reviews')
+      .then(res => {
+        expect(res.body).toHaveLength(100);
+      });
+  });
+
+  it('deletes a review by id', async() => {
+    const review = await getReview();
+    return request(app)
+      .delete(`/api/v1/reviews/${review._id}`)
+      .then(res => {
+        expect(res.body).toEqual(review);
+      });  
   });
 });
